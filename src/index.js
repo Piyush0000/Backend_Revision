0ecf3fae876cc7f6ccb6557567cdde4c72435f43
Piyush <rathorepiyush0000@gmail.com>
2025-06-13 18:49:03 +0530

    Add API error handling and backend setup improvements

--- src/index.js
+++ src/index.js
@@ -5,7 +5,36 @@ import connectDB from "./db/index.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+.then(()=>{
+   app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 3000}`);
+   });
+})
+.catch((err)=>{
+   console.error("Error connecting to MongoDB:", err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 import express from "express";
